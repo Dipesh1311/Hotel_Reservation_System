8bead1a23b037177b75fd3952c6b506ae07388bd
Dipesh1311 <77873151+Dipesh1311@users.noreply.github.com>
2022-07-11 19:36:42 +0530

    Delete HotelReservationSystem.java

--- src/com/bridgelabz/HotelReservationSystem/HotelReservationSystem.java
+++
@@ -1,9 +0,0 @@
-package com.bridgelabz.HotelReservationSystem;
-
-public class HotelReservationSystem {
-
-    public static void main(String[] args) {
-
-        System.out.println("Welcome To Hotel Reservation");
-    }
-}
